26641b79a4635cb5f1ab949b2d57f6efd7ad4aa7
LachinFernando <lachin.fernando@pyxeda.ai>
2025-06-02 06:19:55 +0530

    update(slates):add editable dataframes

--- pages/00_substrate_slate.py
+++ pages/00_substrate_slate.py
@@ -14,6 +14,21 @@ SUBSTRATE_IMAGE = "substrate.png"
 SUBSTRATE_CSV = "substrate.csv"
 SUBSTRATE_EXCEL = "substrate.xlsx"
 
+if "dataframe" not in st.session_state:
+    st.session_state.dataframe = False
+
+if "substrate_df" not in st.session_state:
+    st.session_state.substrate_df = None
+
+if "image" not in st.session_state:
+    st.session_state.image = None
+
+def interacting_editable_df():
+    st.session_state.dataframe = True
+
+def off_interacting_editable_df():
+    st.session_state.dataframe = False
+    st.session_state.image = None
 
 def upload_bucket_path(user_name: str, user_id:str, type_: str, slate_type: str, data_id: str) -> str:
     user_names = user_name.split(" ")
@@ -43,29 +58,36 @@ def substrate_slate():
         "Upload Substrate Image",
         type=["jpg", "jpeg", "png"],
         key="substrate_uploader",
+        on_change=off_interacting_editable_df
     )
     
     if uploaded_substrate is not None:
-        image = handle_image_orientation(Image.open(uploaded_substrate))
-        st.sidebar.image(image, caption="Uploaded Substrate Image")
-        save_uploaded_image(image, SUBSTRATE_IMAGE)
-        
-        with st.spinner("Generating Substrate Labels", show_time=True):
-            substrate_labels = image_label_generator(SUBSTRATE_IMAGE)
-            st.toast("Substrate Labels Generated")
-            substrate_df = create_substrate_dataframe(substrate_labels.model_dump(), SUBSTRATE_CSV)
-            substrate_excel_creation(substrate_labels.model_dump(), SUBSTRATE_EXCEL)
-            data_id = str(uuid.uuid4())
-            upload_to_s3(SUBSTRATE_EXCEL, upload_bucket_path(st.experimental_user['name'], st.experimental_user['sub'], 'excel', 'substrate', data_id))
-            upload_to_s3(SUBSTRATE_IMAGE, upload_bucket_path(st.experimental_user['name'], st.experimental_user['sub'], 'image', 'substrate', data_id))
+        if not st.session_state.dataframe: 
+            image = handle_image_orientation(Image.open(uploaded_substrate))
+            st.session_state.image = image
+            save_uploaded_image(image, SUBSTRATE_IMAGE)
             
-            st.download_button(
-                label="Download as Excel",
-                data=load_and_prepare_excel_for_substrate(SUBSTRATE_EXCEL),
-                file_name='substrate_data.xlsx',
-                mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                on_click='ignore'
-            )
+            with st.spinner("Generating Substrate Labels", show_time=True):
+                substrate_labels = image_label_generator(SUBSTRATE_IMAGE)
+                st.toast("Substrate Labels Generated")
+                print(substrate_labels.model_dump())
+                substrate_df = create_substrate_dataframe(substrate_labels.model_dump(), SUBSTRATE_CSV)
+                st.session_state.substrate_df = substrate_df
+                substrate_excel_creation(substrate_labels.model_dump(), SUBSTRATE_EXCEL)
+                data_id = str(uuid.uuid4())
+                upload_to_s3(SUBSTRATE_EXCEL, upload_bucket_path(st.experimental_user['name'], st.experimental_user['sub'], 'excel', 'substrate', data_id))
+                upload_to_s3(SUBSTRATE_IMAGE, upload_bucket_path(st.experimental_user['name'], st.experimental_user['sub'], 'image', 'substrate', data_id))
+        st.sidebar.image(st.session_state.image, caption="Uploaded Substrate Image")
+        # editable df 
+        edited_df = st.data_editor(st.session_state.substrate_df, on_change=interacting_editable_df)
+        print(edited_df.to_dict())
+        # st.download_button(
+        #     label="Download as Excel",
+        #     data=load_and_prepare_excel_for_substrate(SUBSTRATE_EXCEL),
+        #     file_name='substrate_data.xlsx',
+        #     mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        #     on_click='ignore'
+        # )
 
 if __name__ == "__main__":
     substrate_slate()
--- pages/01_fish_invert_slate.py
+++ pages/01_fish_invert_slate.py
@@ -14,6 +14,22 @@ FISH_INVERT_IMAGE = "fish_and_invert.png"
 FISH_INVERT_CSV = "fish_and_invert.csv"
 FISH_INVERT_EXCEL = "fish_and_invert.xlsx"
 
+if "dataframe" not in st.session_state:
+    st.session_state.dataframe = False
+
+if "substrate_df" not in st.session_state:
+    st.session_state.substrate_df = None
+
+if "image" not in st.session_state:
+    st.session_state.image = None
+
+def interacting_editable_df():
+    st.session_state.dataframe = True
+
+def off_interacting_editable_df():
+    st.session_state.dataframe = False
+    st.session_state.image = None
+
 
 def upload_bucket_path(user_name: str, user_id:str, type_: str, slate_type: str, data_id: str) -> str:
     user_names = user_name.split(" ")
@@ -43,29 +59,36 @@ def fish_invert_slate():
         "Upload Fish and Invert Image",
         type=["jpg", "jpeg", "png"],
         key="fish_invert_uploader",
+        on_change=off_interacting_editable_df
     )
     
     if uploaded_fish_invert is not None:
-        image = handle_image_orientation(Image.open(uploaded_fish_invert))
-        st.sidebar.image(image, caption="Uploaded Fish and Invert Image")
-        save_uploaded_image(image, FISH_INVERT_IMAGE)
-        
-        with st.spinner("Generating Fish and Invert Labels", show_time=True):
-            fish_and_invert_labels = image_label_generator_fish_invert(FISH_INVERT_IMAGE)
-            st.toast("Fish and Invert Labels Generated")
-            fish_and_invert_df = create_fish_slate_dataframe(fish_and_invert_labels.model_dump(), FISH_INVERT_CSV)
-            fish_slate_excel_creation(fish_and_invert_labels.model_dump(), FISH_INVERT_EXCEL)
-            data_id = str(uuid.uuid4())
-            upload_to_s3(FISH_INVERT_EXCEL, upload_bucket_path(st.experimental_user['name'], st.experimental_user['sub'], 'excel', 'fish_and_invert', data_id))
-            upload_to_s3(FISH_INVERT_IMAGE, upload_bucket_path(st.experimental_user['name'], st.experimental_user['sub'], 'image', 'fish_and_invert', data_id))
+        if not st.session_state.dataframe:
+            image = handle_image_orientation(Image.open(uploaded_fish_invert))
+            st.session_state.image = image
+            save_uploaded_image(image, FISH_INVERT_IMAGE)
             
-            st.download_button(
-                label="Download as Excel",
-                data=load_and_prepare_excel_for_fish_slate(FISH_INVERT_EXCEL),
-                file_name='fish_and_invert_data.xlsx',
-                mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-                on_click='ignore'
-            )
+            with st.spinner("Generating Fish and Invert Labels", show_time=True):
+                fish_and_invert_labels = image_label_generator_fish_invert(FISH_INVERT_IMAGE)
+                st.toast("Fish and Invert Labels Generated")
+                fish_and_invert_df = create_fish_slate_dataframe(fish_and_invert_labels.model_dump(), FISH_INVERT_CSV)
+                st.session_state.fish_invert_df = fish_and_invert_df
+                fish_slate_excel_creation(fish_and_invert_labels.model_dump(), FISH_INVERT_EXCEL)
+                data_id = str(uuid.uuid4())
+                upload_to_s3(FISH_INVERT_EXCEL, upload_bucket_path(st.experimental_user['name'], st.experimental_user['sub'], 'excel', 'fish_and_invert', data_id))
+                upload_to_s3(FISH_INVERT_IMAGE, upload_bucket_path(st.experimental_user['name'], st.experimental_user['sub'], 'image', 'fish_and_invert', data_id))
+
+        st.sidebar.image(st.session_state.image, caption="Uploaded Fish and Invert Image")
+        # editable df 
+        edited_df = st.data_editor(st.session_state.fish_invert_df, on_change=interacting_editable_df)
+            
+        # st.download_button(
+        #     label="Download as Excel",
+        #     data=load_and_prepare_excel_for_fish_slate(FISH_INVERT_EXCEL),
+        #     file_name='fish_and_invert_data.xlsx',
+        #     mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        #     on_click='ignore'
+        # )
 
 if __name__ == "__main__":
     fish_invert_slate()
